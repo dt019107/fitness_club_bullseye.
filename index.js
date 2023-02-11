55182a9200f05ec14711b4ffe97f4faff0d720a8
deepak_tiwari <113583232+dt019107@users.noreply.github.com>
2023-02-11 12:22:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import BookForm from "./app.js";
+import "./index.css";
+
+ReactDOM.render(<BookForm />, document.getElementById("root"));
